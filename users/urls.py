04cdf2c727b347ad460507bf440548e2445a98cd
Jean de Dieu UWINTWALI <uwintwalijeandedieu3@gmail.com>
2025-03-25 23:47:47 +0200

    feat: fix profile upload

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.user_list, name='user_list'),
     path('create/', views.user_create, name='user_create'),
-    path('update/<int:user_id>/', views.user_update, name='user_update'),
+    path('update/<int:pk>/', views.user_update, name='user_update'),
     path('delete/<int:user_id>/', views.user_delete, name='user_delete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -6,26 +6,6 @@ def user_list(request):
     users = User.objects.all()
     return render(request, 'users/user_list.html', {'users': users})
 
-def user_create(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-    return redirect('user_list')
-
-
-def user_update(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            return redirect('user_list')
-    else:
-        form = UserForm(instance=user)
-    return render(request, 'users/user_form.html', {'form': form})
-
-
 
 
 def user_delete(request, user_id):
@@ -34,3 +14,31 @@ def user_delete(request, user_id):
         user.delete()
         return redirect('user_list')
     return redirect('user_list')  # Just a fallback for any GET requests
+
+
+
+
+
+def user_create(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES) 
+        if form.is_valid():
+            form.save()
+            return redirect('user_list') 
+    else:
+        form = UserForm()
+    return render(request, 'users/user_form.html', {'form': form})
+
+
+def user_update(request, pk):
+    user = get_object_or_404(User, pk=pk)  # Handle the case where user does not exist
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES, instance=user)  # Ensure to pass request.FILES
+        if form.is_valid():
+            form.save()  # Save the updated user data
+            return redirect('user_list')  
+    else:
+        form = UserForm(instance=user)  # Pre-fill the form with the current user data
+
+    return render(request, 'users/user_form.html', {'form': form, 'user': user})
+
